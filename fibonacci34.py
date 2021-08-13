75e7f368662981ebbf9d9c87f75bcc1f7fe1fe02
Heather <atrytone1650@gmail.com>
2021-08-13 16:22:41 -0700

    Re-creating fibonacci21 for 34, using fiboarray(10)

--- fibonacci34.py
+++ fibonacci34.py
@@ -4,7 +4,9 @@ from functions import *
 class Start(Scene):
 
     def construct(self):
-        fibo=fiboarray(10)
+        i=1
+        fibo = fiboarray(10)
+        last = len(fibo) #keeps track of the length of the fibo array
         self.camera.background_color = WHITE
         dots = [Dot().set_color(RED).move_to(UP * 0.25 * (10-i)) for i in range(0,fibo[-1])]
 
@@ -14,3 +16,9 @@ class Start(Scene):
         t2 = VGroup()
         t3 = VGroup()
         t4 = VGroup()
+
+        for n in range(1,last):
+            t1.add(Text(str(fibo[last - n])).set_color(RED))
+            t2.add(Text(str(fibo[last - n])).set_color(RED))
+            t3.add(Text(str(fibo[last - n - i])).set_color(BLUE))
+            t4.add(Text(str(fibo[last - n - i])).set_color(BLUE))
