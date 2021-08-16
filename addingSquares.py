b88855d053d3847d135a58f107c5c45890a09b5b
gavin4d <gavin4d05@gmail.com>
2021-08-16 12:33:23 -0700

    adding squares updated with camera zoom

--- addingSquares.py
+++ addingSquares.py
@@ -2,14 +2,13 @@ from manim import *
 from functions import *
 import color
 
-class AddingSquares(Scene):
+class AddingSquares(MovingCameraScene):
 
     def construct(self):
         self.camera.background_color = color.BACKGROUND
-        print('First fibonacci (F value):')
+        print('First fibonacci (n value):')
         n = int(input())
         fibo = fiboarray(2*n+2)
-
 
         dotGroup1 = VGroup()
         for i in range(0, pow(fibo[n],2)):
@@ -24,8 +23,8 @@ class AddingSquares(Scene):
         symbols = VGroup().add(Text('+').move_to(LEFT * (4 - (fibo[n] * 0.125) + fibo[n+1] * 0.125)/2).set_color(BLACK)).add(Text('=').move_to(RIGHT * (4 + fibo[n+1] * 0.125)/2).set_color(BLACK))
 
         labels = VGroup()
-        labels.add(Text(str(fibo[n])).set_color(BLACK).move_to(LEFT * 4 + DOWN * 0.25 * (fibo[n+1]/2 + 4)))
-        labels.add(Text(str(fibo[n+1])).set_color(BLACK).move_to(DOWN * 0.25 * (fibo[n+1]/2 + 4)))
+        labels.add(Tex('$' + str(fibo[n]) + '^2$').set_color(BLACK).move_to(LEFT * 4 + DOWN * 0.25 * (fibo[n+1]/2 + 4)))
+        labels.add(Tex('$' + str(fibo[n+1]) + '^2$').set_color(BLACK).move_to(DOWN * 0.25 * (fibo[n+1]/2 + 4)))
         labels.add(Tex('$f_' + str(n) + '$').set_color(BLACK).move_to(LEFT * 4 + DOWN * 0.25 * (fibo[n+1]/2 + 8)))
         labels.add(Tex('$f_' + str(n+1) + '$').set_color(BLACK).move_to(DOWN * 0.25 * (fibo[n+1]/2 + 8)))
 
@@ -35,10 +34,15 @@ class AddingSquares(Scene):
         dotGroup1Copy = dotGroup1.copy()
         dotGroup2Copy = dotGroup2.copy()
 
-        labels.add(Text(str(fibo[2*n+1])).set_color(BLACK).move_to(RIGHT * 6 + UP * 0.5))
+        labels.add(Tex(str(fibo[2*n+1])).set_color(BLACK).move_to(RIGHT * 6 + UP * 0.5))
         labels.add(Tex('$f_' + str(2*n + 1) + '$').set_color(BLACK).move_to(RIGHT * 6 + DOWN * 0.5))
         brace = BraceBetweenPoints(RIGHT * 5 + UP * 0.25 * fibo[2*n+1]/2, RIGHT * 5 + DOWN * 0.25 * fibo[2*n+1]/2).set_color(BLACK).rotate(PI)
 
-        self.play(FadeIn(symbols[1], labels[4], labels[5], brace), dotGroup1Copy.animate.arrange(DOWN * 0.375).move_to(RIGHT * 4 + UP * 0.1274 * (fibo[2*n+1] - pow(fibo[n], 2))), dotGroup2Copy.animate.arrange(DOWN * 0.375).move_to(RIGHT * 4 + DOWN * 0.1274 * (fibo[2*n+1] - pow(fibo[n+1], 2))))
+        self.camera.frame.save_state()
+        camScale = 1
+        if n == 4:
+            camScale = 1.2
+
+        self.play(self.camera.frame.animate.scale(camScale), FadeIn(symbols[1], labels[4], labels[5], brace), dotGroup1Copy.animate.arrange(DOWN * 0.375).move_to(RIGHT * 4 + UP * 0.1274 * (fibo[2*n+1] - pow(fibo[n], 2))), dotGroup2Copy.animate.arrange(DOWN * 0.375).move_to(RIGHT * 4 + DOWN * 0.1274 * (fibo[2*n+1] - pow(fibo[n+1], 2))))
         self.wait(2)
         self.play(FadeIn(Square().scale(10).set_fill(color.BACKGROUND).set_opacity(1)))
